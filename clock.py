c37ad5928f283612690da669a8e780e712245d6a
skynette <cutejosh2@gmail.com>
2020-06-04 03:21:20 +0100

    ssss

--- clock.py
+++ clock.py
@@ -8,6 +8,6 @@ from send_message import send
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(send, 'interval', seconds=2)
+sched.add_job(main, 'interval', seconds=2)
 
 sched.start()
