b6402870a750d7c1ab0a4ba97027cf49c778100e
skynette <cutejosh2@gmail.com>
2020-06-04 03:20:13 +0100

    big change

--- send_message.py
+++ send_message.py
@@ -1,15 +1,33 @@
 from twilio.rest import Client 
-# update
 
-def send():
-	account_sid = 'AC01fb133ba87d07e1db4f58b64badd3ba' 
-	auth_token = 'd20c7414a64028815a76e2b05f912635' 
-	client = Client(account_sid, auth_token) 
-	 
+
+
+
+
+# Download the helper library from https://www.twilio.com/docs/python/install
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+def create_api_key():
+	global account_sid
+	global auth_token
+	global client
+	account_sid = 'AC01fb133ba87d07e1db4f58b64badd3ba'
+	auth_token = '9ea804149c59721393896ecf712d09d7'
+	client = Client(account_sid, auth_token)
+	# new_key = client.new_keys.create()
+	# print(new_key.sid)
+
+
+def send():	 
 	message = client.messages.create( 
 	                              from_='whatsapp:+14155238886',  
-	                              body='I love you so much ❤❤❤❤❤❤\nI wanna be with you everyday of my life 😘😘😘. sick and tired aggggg',      
+	                              body='works now',      
 	                              to='whatsapp:+2348182336574' 
 	                          ) 
 	 
 	print(message.sid)
+
+create_api_key()
+send()
